f1ffaddd254e7d7e3efe5d1fff37b701062ea643
Addison Omead Amiri <git@addomam.net>
2024-02-23 19:48:31 -0800

    Add autogenerated `pacakges` overlay

--- flake-modules/hotwire/overlays.nix
+++ flake-modules/hotwire/overlays.nix
@@ -4,17 +4,42 @@
   ...
 }: let
   hotwireLib = import ./../../lib.nix {inherit lib;};
+  cfg = config.hotwire.overlays;
 in {
-  options.hotwire.overlays.enable = lib.mkEnableOption "hotwire overlays";
+  options.hotwire.overlays = {
+    enable = lib.mkEnableOption "hotwire overlays";
+    generatePackagesOverlay = lib.mkOption {
+      default = true;
+      example = false;
+      type = lib.types.bool;
+      description = "Whether to generate a `.#overlays.packages` that overlays all the packages of this flake. Depends on hotwire.packages.enable being true.";
+    };
+  };
 
   config = lib.mkMerge [
     (lib.mkIf config.hotwire.enable {
       hotwire.overlays.enable = lib.mkDefault true;
     })
-    (lib.mkIf config.hotwire.overlays.enable {
-      flake.overlays =
-        builtins.mapAttrs (_: import)
-        (hotwireLib.nixFiles (config.hotwire.basePath + "/overlays"));
-    })
+    (lib.mkIf cfg.enable (lib.mkMerge [
+      {
+        flake.overlays =
+          builtins.mapAttrs (_: import)
+          (hotwireLib.nixFiles (config.hotwire.basePath + "/overlays"));
+      }
+      /* # For some reason this causes infinite recursion in NixOS configurations but the other implementation doesn't
+      (lib.mkIf cfg.generatePackagesOverlay {
+        flake.overlays.packages = (_: prev:
+          config.flake.packages."${prev.system}"
+        );
+      })
+      */
+      (lib.mkIf cfg.generatePackagesOverlay {
+        flake.overlays.packages = (final: _:
+          builtins.mapAttrs
+          (name: file: final.callPackage file {})
+          (hotwireLib.nixFiles (config.hotwire.basePath + "/packages"))
+        );
+      })
+    ]))
   ];
 }
