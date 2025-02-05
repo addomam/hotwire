c3500be9cad1eb65c677c6ff8b73b11171aed31d
Addison Omead Amiri <git@addomam.net>
2025-02-05 14:07:40 -0800

    Use new options for setting system in Darwin examples

--- templates/example/darwin-configurations/apple-silicon.nix
+++ templates/example/darwin-configurations/apple-silicon.nix
@@ -1,6 +1,6 @@
 {
   networking.hostName = "appleSilicon";
   system.stateVersion = 4;
-  nixpkgs.system = "aarch64-darwin";
+  nixpkgs.hostPlatform.system = "aarch64-darwin";
   test.enable = true;
 }
--- templates/example/darwin-configurations/intel.nix
+++ templates/example/darwin-configurations/intel.nix
@@ -1,6 +1,6 @@
 {
   networking.hostName = "intel";
   system.stateVersion = 4;
-  nixpkgs.system = "x86_64-darwin";
+  nixpkgs.hostPlatform.system = "x86_64-darwin";
   test.enable = true;
 }
